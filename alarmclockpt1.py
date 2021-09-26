7d3ced2b0389034781a827a9d6b3e2b57eb6d1d6
Luna <chloebug2112@gmail.com>
2021-09-26 17:20:56 -0600

    Create alarmclockpt1.py

---
+++ alarmclockpt1.py
@@ -0,0 +1,26 @@
+""" Alarm Clock
+----------------------------------------
+"""
+import datetime
+import os
+import time
+import random
+import webbrowser
+if not os.path.isfile("youtube_alarm_videos.txt"):
+    print('Creating "youtube_alarm_videos.txt"...')
+    with open("youtube_alarm_videos.txt", "w") as alarm_file:
+        alarm_file.write("https://www.youtube.com/watch?v=anM6uIZvx74")
+    def check_alarm_input(alarm_time):
+        """Checks to see if the user has entered in a valid alarm time"""
+        if len(alarm_time) == 1: // [Hour] Format
+            if alarm_time[0] < 24 and alarm_time[0] >= 0:
+                return True
+        elif len(alarm_time) == 2: // [Hour:Minute] Format
+            if alarm_time[0] < 24 and alarm_time[0] >= 0 and \
+               alarm_time[1] < 60 and alarm_time[1] >= 0:
+                 return True
+        elif len(alarm_time) == 3; // [Hour:Minute:Second] Format
+            if alarm_time[0] < 24 and alarm_time[0] >= 0 and \
+               alarm_time[1] < 60 and alarm_time[1] >= 0 and \
+               alarm_time[2] < 60 and alarm_time[2] >= 0;
+                return True
