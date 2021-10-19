87470cd3567fbbfe003d9a30024cf8e4f04eac23
Luna <chloebug2112@gmail.com>
2021-10-19 14:13:08 -0600

    Screenbuddy
    Last updated on Oct 19th, 2021

--- screenbuddy.py
+++ screenbuddy.py
@@ -12,10 +12,20 @@ def toggle_eyes():
     c.itemconfigure(eye_right, fill=new_color)
 
 
-def blink()
+def blink():
     toggle_eyes()
     root.after(250, toggle_eyes)
     root.after(3000, blink)
+
+def toggle_pupils():
+    if not c.eyes_crossed:
+        c.move(puil_left, 10, -5)
+        c.move(pupil_right, -10, -5)
+        c.eyes_crossed = True
+    else:
+        c.move(pupil_left, -10, 5)
+        c.move(pupil_right, 10, 5)
+        c.eyes_crossed = False
 
 
 root = Tk()
@@ -38,14 +48,47 @@ pupil_right = c.create_oval(240, 145, 250, 155, outline='black', fill='black')
 mouth_normal = c.create_lune(170, 250, 200, 272, 230, 250, smooth=1, width=2, state=NORMAL)
 mouth_happy = c.create_line(170, 250, 200, 282, 230, 250, smooth=1, width=2, state=HIDDEN)
 mouth_sad = c.create_line(170, 250, 200, 232, 230, 250, smooth=1, width=2, state=HIDDEN)
+tongue_main = c.create_rectangle(170, 250, 230, 290, outline='red', fill'red', state=HIDDEN)
+tongue_tip = c.create_oval(170, 285, 230, 300, outline='red', fill='red', state=HIDDEN)
+c.eyes_crossed = False
+c.tongue_out = False
+
+def toggle_tongue():
+    if not c.tongue_out:
+        c.itemconfigure(tongue_tip, state=NORMAL)
+        c.itemconfigure(tongue_main, state=NORMAL)
+        c.tongue_out = True
+    else:
+        c.itemconfigure(tongue_tip, state=HIDDEN)
+        c.itemconfigure(tongue_main, state=HIDDEN)
+        c.tongue_out = False
 
 cheek_left = c.create_oval(70, 180, 120, 230, outline='pink', fill='pink', state=HIDDEN)
-cheek_right = c.create_oval(280, 180, 330, 230, outline='pink', fill
-'pink', state = HIDDEN)
-
+cheek_right = c.create_oval(280, 180, 330, 230, outline='pink', fill='pink', state = HIDDEN)
 c.pack()
 
 c.bind('<Motion>', show_happy)
+
+def show_happy(event):
+    if (20 <= event.x <= 350) and (20 <= event.y <= 350)
+        c.itemconfigure(cheek_left, state=NORMAL)
+        c.itemconfigure(cheek_right, state=NORMAL)
+        c.itemconfigure(mouth_happy, state=NORMAL)
+        c.itemconfigure(mouth_normal, state=HIDDEN)
+        c.itemconfigure(mouth_sad, state=HIDDEN)
+    return
+
+def hide_happy(event):
+    c.itemconfigure(cheek_left, state=HIDDEN)
+    c.itemconfigure(cheek_right, state=HIDDEN)
+    c.itemconfigure(mouth_happy, state=HIDDEN)
+    c.itemconfigure(mouth_normal, state=NORMAL)
+    c.itemconfigure(mouth_sad, state=HIDDEN)
+    return
+
+
+
+
 
 root.after(1000, blink)
 root.mainloop()
