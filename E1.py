623ed922e7ad18c102853faa0bfbe163c7efaae3
Luna <chloebug2112@gmail.com>
2021-11-08 08:04:54 -0600

    LPTHW
    The next set of uploads will strictly be from 'Learning python the Hard way', up until E153.

---
+++ E1.py
@@ -0,0 +1,7 @@
+# I'm creating comments here using a hashtag, it allows me to make a comment without it actually showing when the script is run.
+# Comments like these will be ignored by python.
+
+print("Where's the gabagoo?")
+
+# you can also use comments to disable code!
+# print("You won't be able to see this.")
---
+++ E2.py
@@ -0,0 +1,18 @@
+print("How many players are there in our two classes?")
+
+print("Paladin",30 + 45 / 3)
+print("Mage", 20 - 5 * 2)
+
+print("It's time to count our neutral items.")
+
+print(5 + 6 + 2 * 4 -7)
+
+print("Is it true that 6 + 4 > 1 + 5?")
+
+print(6 + 2 > 1 + 5)
+
+print("What is the sum of 6 + 2?", 3 +2)
+print("What is the sum of 1 + 5?", 1 + 5)
+
+print("Use your brain. There's a reason why it's False.")
+
