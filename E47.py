d07ef131703d6471195315a635c8bc7a6d0da725
Luna <chloebug2112@gmail.com>
2021-11-12 13:52:05 -0600

    Hand in hand applications

---
+++ E47.py
@@ -0,0 +1,12 @@
+class Room(object):
+
+    def __init__(selfself, name, description):
+        self.name = name
+        self.description = description
+        self.paths = {}
+
+        def go(self, directions):
+            return self.paths.get.(directiom None)
+
+        def add_paths(self, paths):
+            self.paths.update(paths)
